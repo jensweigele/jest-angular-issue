a381135547fc85c3e4321bf5379019cdf7b4c572
jens.weigele <jens.weigele@adbsafegate.com>
2021-02-25 17:16:59 +0100

    initial commit

---
+++ apps/jest-repro/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jest-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'jest-repro';
+}
